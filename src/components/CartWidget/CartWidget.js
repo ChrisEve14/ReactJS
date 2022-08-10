0b67b89b0ba630fbe87b34d3da5a80d3fa95e98e
Chris Eve Parravicini <chris.parravicini@gmail.com>
2022-08-09 23:24:17 -0300

    cart context css

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,3 +1,4 @@
+import './CartWidget.css'
 import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { CartContext } from '../../context/CartContext'
@@ -10,7 +11,7 @@ const CartWidget = () => {
     const quantity = getQuantity()
 
     return(
-        <Link to='/cart'>
+        <Link to='/cart' className="CartWidget">
             <img style={{width:"85px"}} src='./images/cart.png' alt="cart" />
             {quantity}
         </Link>
